5230ec5af283e6dd66a0fac27a4ae5323e260ca7
Filip Cudny <filip96zx@gmail.com>
2022-01-22 21:51:38 +0100

    added dummy-data

---
+++ src/dummy-data/vehicle.js
@@ -0,0 +1,45 @@
+class Vehicle {
+  constructor() {
+    this.discriminator = "vehicle";
+    this.platesNumber = "WZPV001";
+    this.sideNumber = "Z3-PVAN-01";
+    this.color = "White";
+    this.type = Math.random() > 0.7 ? "TRUCK" : "CAR";
+    this.picture = {
+      id: "e7ace1de-ab7f-4120-922d-23441a041bd9",
+      name: "e7ace1de-ab7f-4120-922d-23441a041bd9",
+      extension: null,
+      contentType: null
+    };
+    this.rangeKm = Math.floor(Math.random() * 400);
+    this.batteryLevelPct = Math.floor(Math.random() * 100);
+    this.reservationEnd = null;
+    this.reservation = null;
+    this.status = Math.random() > 0.5 ? "AVAILABLE" : 'INACCESSIBLE';
+    this.locationDescription = null;
+    this.address = null;
+    this.mapColor = {
+      rgb: "ffffff",
+      alpha: 0.5
+    };
+    this.promotion = null;
+    this.id = new Date().toISOString()+Math.random();
+    this.name = "Enigma Python Van";
+    this.description = null;
+    this.location = {
+      latitude: 52.1935161702226 + (Math.random() - 0.5/5),
+      longitude: 20.9304286193486 + (Math.random() - 0.5/5)
+    };
+    this.metadata = null;
+  }
+
+}
+
+export function getXDummyVehicles(count) {
+  const list = [];
+
+  for (let i = 0; i < count; i++) {
+    list.push(new Vehicle());
+  }
+  return list;
+}
