92f7a31b81b27cd7c7b59f1d1733e02d80f443e6
Filip Cudny <filip96zx@gmail.com>
2022-01-22 22:05:27 +0100

    feat: added vehicles markers icons by filters

--- src/components/mapWindow/marker.js
+++ src/components/mapWindow/marker.js
@@ -1,14 +1,15 @@
 import { Marker as GoogleMarker } from "@react-google-maps/api";
-import { useRef, useState } from 'react';
+import { useRef, useState, useContext } from 'react';
 import { setVehicleStatusIcon, setVehicleBatteryIcon, setVehicleDefaultIcon } from '../../helpers/vehicleMarkers';
 import InfoWindow from "./infoWindow";
-
+import VehiclesContext from "../../globalState/vehiclesContext";
 
 
 function Marker(props) {
   const { clusterer, data } = props;
   const [showInfoWindow, setShowInfoWindow] = useState(false);
   const markerRef = useRef();
+  const { icons: vehiclesIcons } = useContext(VehiclesContext);
 
   const convertLocation = (location) => {
     return {
@@ -25,13 +26,26 @@ function Marker(props) {
     setShowInfoWindow(false);
   };
 
-  if (data.discriminator.toLowerCase() === 'vehicle') {
 
+  if (data.discriminator.toLowerCase() === 'vehicle') {
+    let iconUrl;
+
+    if ('battery' === vehiclesIcons) {
+      iconUrl = setVehicleBatteryIcon(data.batteryLevelPct);
+    } else if ('status' === vehiclesIcons) {
+      iconUrl = setVehicleStatusIcon(data.status);
+    } else {
+      iconUrl = setVehicleDefaultIcon();
+    }
+
+    const icon = {
+      url: iconUrl,
+      scaledSize: new window.google.maps.Size(40, 40)
+    };
     return (
-      <GoogleMarker ref={markerRef} onClick={openInfoWindowHandler} position={convertLocation(data.location)} clusterer={clusterer} icon={setVehicleStatusIcon(data.status)}>
+      <GoogleMarker ref={markerRef} onClick={openInfoWindowHandler} position={convertLocation(data.location)} clusterer={clusterer} icon={icon}>
         {showInfoWindow && <InfoWindow onCloseClick={closeInfoWindowHandler} anchor={markerRef.current} data={data} />}
       </GoogleMarker>
-
     );
   }
 }
