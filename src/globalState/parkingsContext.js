b2d5b2ce7cfd934793fe26c00ebb464166c25166
Filip Cudny <filip96zx@gmail.com>
2022-01-24 15:14:27 +0100

    feat: added parkingsContext

--- src/globalState/parkingsContext.js
+++ src/globalState/parkingsContext.js
@@ -1,13 +1,14 @@
 import React, { createContext, useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
 import { apiURL } from '../config/config';
+import useFetchMapObj from '../helpers/apiFetchMapObj';
 
 const ParkingsContext = createContext({
   data: [],
   icons: 'default',
   fetchData: () => { },
-  filterStatus: () => { },
-  filterBattery: () => { },
-  filterType: () => { },
+  filterFreeSpaces: () => { },
+  filterChargers: () => { },
   setIconsBy: () => { },
   isLoading: true,
   error: null
@@ -15,35 +16,84 @@ const ParkingsContext = createContext({
 
 export default function ParkingsContextProvider(props) {
   const [data, setData] = useState([]);
-  const [fetchedData, setFetchedData] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const { data: fetchedData, fetchData, error, isLoading } = useFetchMapObj('PARKING');
 
+  const [icons, setIcons] = useState('default');
 
-  const fetchData = async () => {
-    setIsLoading(true);
-    try {
-      const data = await fetch(apiURL+'map?objectType=PARKING').then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw Error('Error !');
-        }
-      });
-      const vehiclesList = data.objects.map(item => item);
-      setFetchedData(vehiclesList);
-      setError(null);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsLoading(false);
+  const [filters, setFilters] = useState([
+    { property: 'availableSpacesCount', value: 'all' },
+    { property: 'chargers', value: 'all' }
+  ]);
+
+  useEffect(() => {
+    let filteredData = [...fetchedData];
+    const spacesFilter = filters.find(filter => filter.property === 'availableSpacesCount');
+    const chargersFilter = filters.find(filter => filter.property === 'chargers');
+
+    if (spacesFilter.value === 'free') {
+      filteredData = filteredData.filter(parking => parking.availableSpacesCount > 0);
     }
+    if (spacesFilter.value === 'full') {
+      filteredData = filteredData.filter(parking => parking.availableSpacesCount === 0);
+    }
+    if (chargersFilter.value === 'with') {
+      filteredData = filteredData.filter(parking => parking.chargers?.length > 0);
+    }
+    if (chargersFilter.value === 'without') {
+      filteredData = filteredData.filter(parking => parking.chargers?.length === 0);
+    }
+
+    setData(filteredData);
+
+  }, [filters, fetchedData]);
+
+
+  const filterFreeSpaces = (setFreeAllFull) => {
+    const filterNormalized = setFreeAllFull.toLowerCase();
+    setFilters((prevState) => prevState.map(filter => filter.property === 'availableSpacesCount' ?
+      {
+        property: filter.property,
+        value: filterNormalized
+      }
+      :
+      {
+        property: filter.property,
+        value: filter.value
+      }));
+  };
+
+  const filterChargers = (setAllWithWithout) => {
+    const filterNormalized = setAllWithWithout.toLowerCase();
+    setFilters((prevState) => prevState.map(filter => filter.property === 'chargers' ?
+      {
+        property: filter.property,
+        value: filterNormalized
+      }
+      :
+      {
+        property: filter.property,
+        value: filter.value
+      }));
+  };
+
+  const setIconsBy = (iconsBy) => {
+   const normalizedIconsBy = iconsBy.toLowerCase();
+
   };
 
 
 
 
+
   const context = {
+    data: data,
+    icons: icons,
+    fetchData: fetchData,
+    filterFreeSpaces: filterFreeSpaces,
+    filterChargers: filterChargers,
+    setIconsBy: setIconsBy,
+    isLoading: isLoading,
+    error: error
 
   };
 
